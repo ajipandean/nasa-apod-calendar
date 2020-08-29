b648c6acf3477b9cd3f0efd53ed86a1b55046363
Muhamad Aji Pandean Mertayasa <ajikun9@gmail.com>
2020-08-29 14:29:22 +0800

    Add toolbar in layouts-default

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,12 @@
 <template lang="html">
-  <q-layout>
+  <q-layout view="hHh lpR lfr">
+    <q-header elevated>
+      <q-toolbar style="height:64px;">
+        <q-toolbar-title>
+          NASA Astronomy Picture of the Day
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
     <q-page-container>
       <slot/>
     </q-page-container>
